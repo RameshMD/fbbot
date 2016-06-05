3147893b93cf24774a3c895fd4cdb1e413c91075
Ramesh Manthala <ramesh@Rameshs-MacBook.local>
2016-06-05 08:55:06 -0400

    web config commit

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 });
 
+// Facebook validation
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === config.secretKey) {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+});
+
 app.listen(config.port);
